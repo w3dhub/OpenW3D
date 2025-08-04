369720d619588a6302815d2f50f7519b6be1c4c3
Robert Middleton <robert.middleton@rm5248.com>
2025-08-03 20:22:07 -0400

    Replace custom mutex implementation with C++ standard implemenation
    This should be semantically equivalent to the current code.
    
    use std atomic

--- Code/wwlib/mutex.h
+++ Code/wwlib/mutex.h
@@ -26,53 +26,9 @@
 #include "always.h"
 #include "thread.h"
 
+#include <mutex>
+#include <atomic>
 
-// Always use mutex or critical section when accessing the same data from multiple threads!
-
-// ----------------------------------------------------------------------------
-//
-// Mutex class is an expensive way of synchronization! Use critical sections
-// (below) for all synchronization. Use mutexes for inter-process locking.
-//
-// ----------------------------------------------------------------------------
-
-class MutexClass
-{
-	void* handle;
-	unsigned locked;
-
-	// Lock and unlock are private so that you can't use them directly. Use LockClass as a sentry instead!
-	// Lock returns true if lock was succesful, false otherwise
-	bool Lock(int time);
-	void Unlock();
-
-public:
-	// Name can (and usually should) be NULL. Use name only if you wish to create a globally unique mutex
-	MutexClass(const char* name = NULL);
-	~MutexClass();
-
-	enum {
-		WAIT_INFINITE=-1
-	};
-
-	class LockClass
-	{
-		MutexClass& mutex;
-		bool failed;
-	public:
-
-		// In order to lock a mutex create a local instance of LockClass with mutex as a parameter.
-		// Time is in milliseconds, INFINITE means infinite wait.
-		LockClass(MutexClass& m, int time=MutexClass::WAIT_INFINITE);
-		~LockClass();
-
-		// Returns true if the lock failed
-		bool Failed() { return failed; }
-	private:
-		LockClass &operator=(const LockClass&) { return(*this); }
-	};
-	friend class LockClass;
-};
 
 // ----------------------------------------------------------------------------
 //
@@ -83,8 +39,7 @@ public:
 
 class CriticalSectionClass
 {
-	void* handle;
-	unsigned locked;
+    std::recursive_mutex mtx;
 
 	// Lock and unlock are private so that you can't use them directly. Use LockClass as a sentry instead!
 	void Lock();
@@ -114,40 +69,40 @@ public:
 // of it is that it can't be locked multiple times from the same thread.
 //
 // ----------------------------------------------------------------------------
-
+// 2025-08: note: this is /allegedly/ faster than the CriticalSectionClass.
+// The assumptions made circa 2000 may no longer hold.
 class FastCriticalSectionClass
 {
-	volatile unsigned Flag;
+#ifdef FAST_CRITICAL_IS_MUTEX
+    std::mutex mtx;
+#else
+    std::atomic_flag Flag{};
+#endif
 
 	void Thread_Safe_Set_Flag()
 	{
-		volatile unsigned& nFlag=Flag;
-
-		#define ts_lock _emit 0xF0
-		assert(((unsigned)&nFlag % 4) == 0);
-
-		__asm mov ebx, [nFlag]
-		__asm ts_lock
-		__asm bts dword ptr [ebx], 0
-		__asm jc The_Bit_Was_Previously_Set_So_Try_Again
-		return;
-
-		The_Bit_Was_Previously_Set_So_Try_Again:
-		ThreadClass::Switch_Thread();
-		__asm mov ebx, [nFlag]
-		__asm ts_lock
-		__asm bts dword ptr [ebx], 0
-		__asm jc  The_Bit_Was_Previously_Set_So_Try_Again
+#ifdef FAST_CRITICAL_IS_MUTEX
+        mtx.lock();
+#else
+        while (Flag.test_and_set(std::memory_order_acq_rel)) {
+            Flag.wait(true, std::memory_order_relaxed);
+        }
+#endif
 	}
 
-	WWINLINE void Thread_Safe_Clear_Flag()
+    void Thread_Safe_Clear_Flag()
 	{
-		Flag = 0;
+#ifdef FAST_CRITICAL_IS_MUTEX
+        mtx.unlock();
+#else
+        Flag.clear(std::memory_order_release);
+        Flag.notify_one();
+#endif
 	}
 
 public:
 	// Name can (and usually should) be NULL. Use name only if you wish to create a globally unique mutex
-	FastCriticalSectionClass() : Flag(0) {}
+    FastCriticalSectionClass() {}
 
 	class LockClass
 	{
