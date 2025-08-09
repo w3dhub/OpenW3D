38dc1ba30f769e7941384d8e11b20651d5f2c44f
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-08-09 23:55:28 +0200

    bool is available in c++ 20

--- Code/wwlib/bool.h
+++ Code/wwlib/bool.h
@@ -40,24 +40,9 @@
 #if !defined(TRUE_FALSE_DEFINED) && !defined(__BORLANDC__) && (_MSC_VER < 1100) && !defined(__WATCOMC__)
 #define TRUE_FALSE_DEFINED
 
-/**********************************************************************
-**      The "bool" integral type was defined by the C++ comittee in
-**      November of '94. Until the compiler supports this, use the following
-**      definition.
-*/
 #ifdef _MSC_VER
 
 #include        "yvals.h"
-#define bool    unsigned
-
-#elif defined(_UNIX)
-
-/////#define bool    unsigned
-
-#else
-
-enum {false=0,true=1};
-typedef int bool;
 
 #endif
 
