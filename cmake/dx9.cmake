ce068a658e1757e3692f4ad25bd8aac30dec1aaa
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-09-29 00:11:22 +0200

    The d3d9 sdk is only needed for MSVC
    The MinGW toolchain has built-in support

--- cmake/dx9.cmake
+++ cmake/dx9.cmake
@@ -1,7 +1,12 @@
-FetchContent_Declare(
-    dx9
-    GIT_REPOSITORY https://github.com/madebr/min-dx9-sdk.git
-    GIT_TAG        55973049eaaab204bb35a2b4e33a129a75a16244
-)
+if(MSVC)
+    FetchContent_Declare(
+        dx9
+        GIT_REPOSITORY https://github.com/madebr/min-dx9-sdk.git
+        GIT_TAG        55973049eaaab204bb35a2b4e33a129a75a16244
+    )
 
-FetchContent_MakeAvailable(dx9)
+    FetchContent_MakeAvailable(dx9)
+else()
+    add_library(d3d9lib INTERFACE)
+    target_link_libraries(d3d9lib INTERFACE d3d9 d3dx9 dinput8 dxguid)
+endif()
