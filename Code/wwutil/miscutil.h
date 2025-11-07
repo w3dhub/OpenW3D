da2717977830c0dbc8f4cff9a8d64d5965cea652
Robert Middleton <rm5248@users.noreply.github.com>
2025-11-06 20:02:15 -0500

    Build wwutil on Linux (#73)

--- Code/wwutil/miscutil.h
+++ Code/wwutil/miscutil.h
@@ -31,31 +31,22 @@
 #ifndef MISCUTIL_H
 #define MISCUTIL_H
 
-#ifndef	ALWAYS_H
-	#include "always.h"
-#endif
-
-#include	"bittype.h"
-#include	"wwstring.h"
-
 const float MISCUTIL_EPSILON = 0.0001f;
 
 class cMiscUtil
 {
 	public:
-		static LPCSTR	Get_Text_Time(void);
+        static const char *	Get_Text_Time(void);
       static void		Seconds_To_Hms(float seconds, int & h, int & m, int & s);
-		static bool		Is_String_Same(LPCSTR str1, LPCSTR str2);
-		static bool		Is_String_Different(LPCSTR str1, LPCSTR str2);
-		static void		Get_File_Id_String(LPCSTR filename, StringClass & str);
-      static bool		File_Exists(LPCSTR filename);
-		static bool		File_Is_Read_Only(LPCSTR filename);
+        static bool		Is_String_Same(const char * str1, const char * str2);
+        static bool		Is_String_Different(const char * str1, const char * str2);
+      static bool		File_Exists(const char * filename);
       static bool		Is_Alphabetic(char c);
       static bool		Is_Numeric(char c);
       static bool		Is_Alphanumeric(char c);
 		static bool		Is_Whitespace(char c);
 		static void		Trim_Trailing_Whitespace(char * text);
-      static void		Remove_File(LPCSTR filename);
+      static void		Remove_File(const char * filename);
 
 	private:
 };
