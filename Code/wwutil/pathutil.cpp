2df83272b045451946d037566993f4787505ba06
Anonymous Maarten <anonymous.maarten@gmail.com>
2026-02-16 23:27:13 +0100

    Introduce cPathUtil::GetWorkingDirectory

--- Code/wwutil/pathutil.cpp
+++ Code/wwutil/pathutil.cpp
@@ -2,6 +2,19 @@
 
 #include <filesystem>
 
+StringClass cPathUtil::GetWorkingDirectory(bool trailing_separator)
+{
+	std::error_code ec;
+	auto path = std::filesystem::current_path(ec);
+	if (ec) {
+		path = ".";
+	}
+	if (trailing_separator) {
+		path += std::filesystem::path::preferred_separator;
+	}
+	return StringClass{path.generic_string().c_str()};
+}
+
 bool cPathUtil::PathExists(const char *path)
 {
 	std::error_code ec;
