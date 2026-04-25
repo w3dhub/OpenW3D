b35dd172af4526348dac63f3f99b2c34e22a42b3
Orbit <orbit@openw3d.local>
2026-04-25 09:07:06 +0000

    fix: add dialogresource.h stub forwarding to renegadedialog.h
    The original dialogresource.h was merged into renegadedialog.h but
    some .cpp files still #include it. Add a forwarding stub to unblock
    the build.

---
+++ Code/Commando/dialogresource.h
@@ -0,0 +1,10 @@
+/*
+** Legacy header - contents moved to renegadedialog.h
+*  $Archive:: /Commando/Code/Commando/dialogresource.h      $
+*/
+
+#pragma once
+
+// This file was the original home for dialog resource IDs.
+// The contents were migrated to renegadedialog.h.
+#include "renegadedialog.h"
