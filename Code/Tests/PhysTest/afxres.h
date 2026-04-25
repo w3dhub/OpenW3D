26c7c3e61385f0ae7ab60f917501b333adbe53f0
Orbit <orbit@openw3d.local>
2026-04-25 08:44:12 +0000

    fix: add afxres.h stubs to all directories with .rc files

---
+++ Code/Tests/PhysTest/afxres.h
@@ -0,0 +1,22 @@
+/*
+** Minimal afxres.h stub for builds without MFC/ATL.
+** Provides the minimal set of symbols required by resource scripts.
+*/
+
+#ifndef __AFXRES_H__
+#define __AFXRES_H__
+
+// Standard resource IDs that resource scripts expect
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_BANDTETYPE                  129
+
+// Common control constants
+#define APSTUDIO_INVOKED               1
+#define APSTUDIO_HIDDEN_SYMBOLS        1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+
+#endif // __AFXRES_H__
