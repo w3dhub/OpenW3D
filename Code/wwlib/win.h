02900059289c95ab1340a3b8466a102fce8d6747
Dadud <dadud@users.noreply.github.com>
2026-04-29 13:20:09 +0000

    Commando: replace Win32 window with SDL3 window
    When OPENW3D_SDL3 is defined, create an SDL3 window instead of a Win32
    window using SDL_CreateWindow. Add SDL3_Pump_Events for focus handling.
    Conditionally compile Main_Window_Proc and related Win32 code out when
    using SDL3.

--- Code/wwlib/win.h
+++ Code/wwlib/win.h
@@ -57,23 +57,25 @@
 //#include	<winnt.h>
 //#include	<winuser.h>
 
+#if defined(OPENW3D_SDL3)
+#include <SDL3/SDL.h>
+extern SDL_Window *MainWindow;
+#else
 #ifdef _WIN32
 extern HINSTANCE	ProgramInstance;
 extern HWND			MainWindow;
 extern bool GameInFocus;
+#endif
+#endif
 
 #ifdef _DEBUG
-
+#ifdef _WIN32
 void __cdecl Print_Win32Error(unsigned int win32Error);
-
-#else // _DEBUG
-
+#else
 #define Print_Win32Error
-
-#endif // _DEBUG
-
-#else // _WIN32
-//#include <unistd.h>
-#endif // _WIN32
+#endif
+#else // _DEBUG
+#define Print_Win32Error
+#endif
 
 #endif // WIN_H
